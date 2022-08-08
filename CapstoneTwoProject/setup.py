7f77f252c66d29f88828b1eba3c49c04d0128f3d
Panda Bear <pandabear@MacBook-Air.local>
2022-08-08 09:47:04 -0700

    first commit

---
+++ CapstoneTwoProject/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting Market Rent price in the SF/BayArea',
+    author='Melissa Han',
+    license='MIT',
+)
